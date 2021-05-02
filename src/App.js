daa0c17e346e2465f5d3b4bbfb2d27fc1e50d27c
Jakub Maniak <punisher4ce@gmail.com>
2021-05-02 19:07:59 +0200

    Add an outline of web page layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Video Effects App
+    <div className="app">
+      <div className="app__column">
+          <section className="app-section">
+            <header>
+              <h1 className="app-section__title">Video sources</h1>
+            </header>
+            <div className="app-section__container">
+              Video sources content
+            </div>
+          </section>
+          <section className="app-section">
+            <header>
+              <h1 className="app-section__title">Video preview</h1>
+            </header>
+            <div className="app-section__container">
+              Video preview content
+            </div>
+          </section>
+      </div>
+      <div className="app__column">
+        <section className="app-section">
+          <header>
+            <h1 className="app-section__title">Presets</h1>
+            <button>Add new preset</button>
+          </header>
+          <div className="app-section__container">
+            Presets content
+          </div>
+        </section>
+        <section className="app-section">
+          <header>
+            <h1 className="app-section__title">Preset editor</h1>
+          </header>
+          <div className="app-section__container">
+            Preset editor content
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
