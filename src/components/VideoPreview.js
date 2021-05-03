cce278412070133de3b8ac5cc2e42d4a89ba8761
Jakub Maniak <punisher4ce@gmail.com>
2021-05-03 12:07:51 +0200

    Add video preview elements and CSS rules

--- src/components/VideoPreview.js
+++ src/components/VideoPreview.js
@@ -2,7 +2,13 @@ import '../styles/VideoPreview.css';
 
 function VideoPreview() {
     return (
-        <canvas width="640" height="360"></canvas>
+        <div className="video-preview__container">
+            <canvas width="640" height="360"></canvas>
+            <div className="video-controls">
+                <button className="video-control--play"></button>
+                <button className="video-control--volume-medium"></button>
+            </div>
+        </div>
     );
 }
 
